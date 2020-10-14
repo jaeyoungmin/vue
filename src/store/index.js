f91ebc50131b021d9959b8a4abdfdbbd04eb5420
jaeyoungmin <jaeyoung8456@naver.com>
2020-10-14 17:57:39 +0900

    pinterest

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,15 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    images:[]
+  },
+  mutations: {
+    setImage(state, payload){
+      state.images = payload
+      console.log('success!')
+    }
+  },
   actions: {},
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <core-app-bar />
+    <core-view />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    CoreAppBar: () => import('@/components/core/AppBar.vue'),
+    CoreView: () => import('@/components/core/View.vue')
   }
 };
 </script>
+<style>
+a{
+    list-style: none;
+    text-decoration: none;
+  
+}
+</style>
